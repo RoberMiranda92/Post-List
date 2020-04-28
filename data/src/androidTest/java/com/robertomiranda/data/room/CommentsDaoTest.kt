7bb0393e821ca9abfd18d436d66bfc72003a6c5f
Roberto <rmp0046@gmail.com>
2020-04-28 03:54:08 -0400

    fix comments dao test problem with FK

--- data/src/androidTest/java/com/robertomiranda/data/room/CommentsDaoTest.kt
+++ data/src/androidTest/java/com/robertomiranda/data/room/CommentsDaoTest.kt
@@ -3,22 +3,27 @@ package com.robertomiranda.data.room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.robertomiranda.data.BaseDataBaseTest
 import com.robertomiranda.data.room.dao.CommentsDao
+import com.robertomiranda.data.room.dao.PostsDao
 import com.robertomiranda.data.room.models.CommentRoom
+import com.robertomiranda.data.room.models.PostRoom
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class CommentsDaoTest : BaseDataBaseTest() {
 
+    private lateinit var postDao: PostsDao
     private lateinit var commentsDao: CommentsDao
 
     override fun setUpChild() {
         commentsDao = database.commentsDao()
+        postDao = database.postDao()
+        postDao.addPostList(listOf(Post1, Post2, Post3)).test().await()
     }
 
     @Test
-    fun insertAndGetAllPost() {
-        val commentList = mutableListOf<CommentRoom>(
+    fun insertAndGetAllComments() {
+        val commentList = listOf<CommentRoom>(
             Comment1,
             Comment2,
             Comment3
@@ -28,13 +33,13 @@ class CommentsDaoTest : BaseDataBaseTest() {
         val testObserver = commentsDao.getAllComments().test()
 
         testObserver.awaitCount(1)
-        testObserver.assertValue(commentList)
+        testObserver.assertValue { list -> list == commentList }
         testObserver.dispose()
     }
 
     @Test
     fun insertAndGetPostByID() {
-        val commentList = mutableListOf<CommentRoom>(
+        val commentList = listOf<CommentRoom>(
             Comment1,
             Comment2,
             Comment3
@@ -44,11 +49,30 @@ class CommentsDaoTest : BaseDataBaseTest() {
         val testObserver = commentsDao.getCommentByPostId(1).test()
 
         testObserver.awaitCount(1)
-        testObserver.assertValue(commentList.subList(0, 2))
+        testObserver.assertValue { list -> list == commentList.subList(0, 2) }
         testObserver.dispose()
     }
 
     companion object {
+
+        val Post1 = PostRoom(
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            1, 1,
+            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        )
+
+        val Post2 = PostRoom(
+            "qui est esse",
+            1, 2,
+            "est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla"
+        )
+
+        val Post3 = PostRoom(
+            "qui est esse",
+            2, 3,
+            "est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla"
+        )
+
         val Comment1 = CommentRoom(
             "id labore ex et quam laborum",
             1,
